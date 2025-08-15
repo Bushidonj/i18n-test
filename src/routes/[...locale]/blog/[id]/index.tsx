0894e32829457d36bad364ff74c5fbefbc73e098
Allan <70598205+Bushidonj@users.noreply.github.com>
2025-08-15 14:59:42 -0300

    clear

--- src/routes/[...locale]/blog/[id]/index.tsx
+++
@@ -1,7 +0,0 @@
-import { component$ } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
-
-export default component$(() => {
-  const location = useLocation();
-  return <div>Pretend this is the blog text for "{location.params.id}".</div>;
-});
--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { RequestHandler } from "@builder.io/qwik-city";
-import { extractLang } from "~/i18n";
-
-export const onGet: RequestHandler = async ({ request, redirect, url }) => {
-  const guessedLocale = extractLang(request, url);
-  console.log(`  ➜  GET / - Redirecting to /${guessedLocale}...`);
-  throw redirect(301, `/${guessedLocale}/${url.search}`);
-};
